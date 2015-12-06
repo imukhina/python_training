14a7fae3ec636bede9da78aaed29691be036c71d
Irina M <mad_irka@mail.ru>
2015-12-06 17:17:48 +0300

    выделен класс для управления сессиями

--- fixture/application.py
+++ fixture/application.py
@@ -1,13 +1,11 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
+from fixture.session import SessionHelper
 
 class Application:
     def __init__(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
+        self.session = SessionHelper(self)
 
     def create_contact(self, contact):
         wd = self.wd
@@ -80,24 +78,9 @@ class Application:
         wd.find_element_by_name("notes").send_keys(contact.note)
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
-    def login(self, username, password):
-        wd = self.wd
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_xpath("//form[@id='LoginForm']//label[.='Password:']").click()
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_css_selector("input[type=\"submit\"]").click()
-
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
 
     def destroy(self):
         self.wd.quit()
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -14,7 +14,7 @@ def app(request):
 
 def test_test_add_contact(app):
     app.open_home_page()
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Contact(first="First", middle="Middle", last="Last", nick="nick", title="title", company="comp", address="London 212 Baker st", home="home", mob="79031022180", work="352524524", fax="32545245", homephone="3224545", email="fgdfhgdf@fd.jh", homepage="www.ya.ru", birthyear="1984", note="Note"))
-    app.logout()
+    app.session.logout()
 
